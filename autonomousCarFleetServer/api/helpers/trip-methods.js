23acd0cc28c77167ab72451ea76c686fa9b21639
Maïté CAZENAVE <admin@Mac-de-admin.local>
2017-01-18 12:25:07 +0100

    xo

---
+++ autonomousCarFleetServer/api/helpers/trip-methods.js
@@ -0,0 +1,101 @@
+/**
+ * Created by simonthome on 23/12/2016.
+ */
+'use strict';
+
+const Promise = require('bluebird');
+const distance = require('google-distance');
+const async = require('async');
+const Trip = require('../model/trip');
+const carMethods = require('./carMethods');
+
+const tripMethods = {
+	newTrip(trip) {
+		return new Promise((resolve, reject) => {
+			const date = new Date();
+			distance.get({
+				origin: trip.departureAddress,
+				destination: trip.arrivalAddress
+			}, (err, data) => {
+		// const tripData =
+		// console.log('console' + JSON.stringify(tripData));
+				const newTrip = new Trip({
+					accountId: trip.accountId,
+					carId: trip.carId,
+					groupName: '',
+					departureAddress: data.origin,
+					arrivalAddress: data.destination,
+					distance: data.distance,
+					distanceValue: data.distanceValue,
+					duration: data.duration,
+					durationValue: data.durationValue,
+					departureDate: trip.departureDate,
+					creationDate: date
+				});
+				newTrip.save()
+					.then(tripData => resolve(tripData))
+					.catch(err => reject(err));
+			});
+		});
+	},
+	newGroupTrip(trip) {
+		return new Promise((resolve, reject) => {
+			carMethods.findGroupCars(trip.accountId, trip.groupName)
+				.then(cars => {
+					let tempTrips = [];
+					async.each(cars, (car, callback) => {
+						const date = new Date();
+						distance.get({
+							origin: trip.departureAddress,
+							destination: trip.arrivalAddress
+						}, (err, data) => {
+							const newTrip = new Trip({
+								accountId: trip.accountId,
+								carId: car._id,
+								groupName: trip.groupName,
+								departureAddress: data.origin,
+								arrivalAddress: data.destination,
+								distance: data.distance,
+								distanceValue: data.distanceValue,
+								duration: data.duration,
+								durationValue: data.durationValue,
+								departureDate: trip.departureDate,
+								creationDate: date
+							});
+							newTrip.save()
+								.then(tripData => {
+									console.log(JSON.stringify(tripData));
+									tempTrips.push(tripData);
+									callback();
+								})
+								.catch(err => reject(err));
+						});
+					}, err => {
+						if (err) {
+							reject();
+						} else {
+							console.log(JSON.stringify(tempTrips));
+							resolve(tempTrips);
+						}
+					});
+				});
+		});
+	},
+	findAllTrips() {
+		return Trip.find({}).exec();
+	},
+	findTripsByCarId(id) {
+		return Trip.find({carId: id}).exec();
+	},
+	findOneTrip(id) {
+		return Trip.findOne({_id: id}).exec();
+	},
+	deleteOneTrip(id) {
+		return Trip.remove({_id: id}).exec();
+	},
+	updateOneTrip(id, trip) {
+		return Trip.update({_id: id}, trip).exec();
+	}
+};
+
+module.exports = tripMethods;
--- autonomousCarFleetServer/api/routes/client/group-manage.js
+++ autonomousCarFleetServer/api/routes/client/group-manage.js
@@ -4,79 +4,74 @@
 'use strict';
 
 const express = require('express');
-
-const Account = require('../../model/account');
 const Promise = require('bluebird');
-const config = require('../../../config/config');
 const jwt = require('jwt-simple');
-const carMethods = require('../../helpers/carMethods');
 const passport = require('passport');
+const Account = require('../../model/account');
+const config = require('../../../config/config');
+const carMethods = require('../../helpers/carMethods');
 
 module.exports = function () {
-  const router = new express.Router();
-
-  router.patch('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
-    if (!req.params.id) {
-      res.json({success: false, msg: 'missing params'});
-    } else {
-      carMethods.findOneCar(req.params.id)
-        .then(car => {
-          if (car) {
-            car.groupName = req.body.groupName;
-            return carMethods.updateOneCar(car._id, car)
-          } else {
-            res.json({success: false, msg: 'car not found'});
-          }
-        })
-        .then(updateResponse => {
-          console.log(updateResponse);
-          res.json({success: true, msg: 'Successfully updated car'});
-        })
-        .catch(err => {
-          console.log(err);
-          res.json({success: false, msg: 'error updating or finding car'});
-        })
-    }
-  });
-
-  router.patch('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-    const accountId = req.header.accountId;
-    carMethods.findGroupCars(accountId, req.body.groupName)
-      .then(cars => {
-        if (cars.length > 0) {
-          cars.forEach(car => {
-            car.used = true;
-            return carMethods.updateOneCar(car._id, car)
-          })
-        } else {
-          res.json({success: false, msg: 'No cars found in group'});
-        }
-      })
-      .then(updateResponse => {
-        res.json({success: true, msg: 'successfully updated cars in group'});
-      })
-      .catch(err => {
-        res.json({success: false, msg: 'error updating cars in group'});
-      })
-  });
-
-  router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-    const accountId = req.header.accountId;
-    console.log(req.body.groupName);
-    carMethods.findGroupCars(accountId, req.body.groupName)
-      .then(carsArray => {
-        console.log(carsArray);
-        if (carsArray.length > 0) {
-          res.json({success: true, msg: 'Successfully found group cars', cars: carsArray});
-        } else {
-          res.json({succes: false, msg: 'Cars not found'});
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.json({success: false, msg: 'Error getting all cars'});
-      });
-  });
-
-  return router;
+	const router = new express.Router();
+	router.patch('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+		if (!req.params.id) {
+			res.json({success: false, msg: 'missing params'});
+		} else {
+			carMethods.findOneCar(req.params.id)
+				.then(car => {
+					if (car) {
+						car.groupName = req.body.groupName;
+						return carMethods.updateOneCar(car._id, car);
+					} else {
+						res.json({success: false, msg: 'car not found'});
+					}
+				})
+				.then(updateResponse => {
+					console.log(updateResponse);
+					res.json({success: true, msg: 'Successfully updated car'});
+				})
+				.catch(err => {
+					console.log(err);
+					res.json({success: false, msg: 'error updating or finding car'});
+				});
+		}
+	});
+	router.patch('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+		const accountId = req.header.accountId;
+		carMethods.findGroupCars(accountId, req.body.groupName)
+			.then(cars => {
+				if (cars.length > 0) {
+					cars.forEach(car => {
+						car.used = true;
+						return carMethods.updateOneCar(car._id, car)
+					});
+				} else {
+					res.json({success: false, msg: 'No cars found in group'});
+				}
+			})
+			.then(updateResponse => {
+				res.json({success: true, msg: 'successfully updated cars in group'});
+			})
+			.catch(err => {
+				res.json({success: false, msg: 'error updating cars in group'});
+			});
+	});
+	router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+		const accountId = req.header.accountId;
+		console.log(req.body.groupName);
+		carMethods.findGroupCars(accountId, req.body.groupName)
+			.then(carsArray => {
+				console.log(carsArray);
+				if (carsArray.length > 0) {
+					res.json({success: true, msg: 'Successfully found group cars', cars: carsArray});
+				} else {
+					res.json({succes: false, msg: 'Cars not found'});
+				}
+			})
+			.catch(err => {
+				console.log(err);
+				res.json({success: false, msg: 'Error getting all cars'});
+			});
+	});
+	return router;
 };
